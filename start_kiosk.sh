805178f609bf61580dc9ef3328f9f5b46f4f03b1
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 12:43:55 +0200

    add openbox
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker-compose up -d
-chromium-browser --kiosk --start-fullscreen --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic "http://localhost:9000/"
+
+startx /usr/bin/openbox-session -- -nocursor &
+sleep 5
+
+DISPLAY=:0 chromium-browser --kiosk --start-fullscreen --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic "http://localhost:9000/"
