cb93b37d2fb9105d158c4cb9f45a3a38f3f8550e
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 13:47:57 +0200

    revert script
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -5,9 +5,4 @@ sleep 10
 
 export DISPLAY=:0
 
-unclutter &
-
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
-
 chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/"
