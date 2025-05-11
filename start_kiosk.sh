2d4bea3877031da27982732c5293aea427eded64
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 14:54:00 +0200

    remove loop
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -7,11 +7,4 @@ export DISPLAY=:0
 
 unclutter &
 
-while true; do
-    sleep 20
-    if ! pgrep -f chromium-browser > /dev/null; then
-        chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/" &
-    else
-        log "Chromium still running"
-    fi
-done
+chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/" &
