5d39bb3fad686dfa175f5b641a12164e8eafbd60
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 13:34:25 +0200

    change start
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 docker compose up -d
 
-while ! nc -z localhost 9000; do   
-  sleep 1
-done
+sleep 10
 
-timeout 300 bash -c "until xdpyinfo >/dev/null 2>&1; do sleep 1; done"
-
-DISPLAY=:0 chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/"
+chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/"
