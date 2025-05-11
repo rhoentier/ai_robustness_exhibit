51733c2ff43ea437699cf1e8913f5282c514496e
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 13:05:44 +0200

    update script
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -2,4 +2,4 @@
 docker compose up -d
 sleep 10
 
- chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic "http://localhost:9000/"
+ chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/"
