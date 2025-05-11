5b4f3e4d36dd275c4c9cd57420add6b98d62867d
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 13:43:06 +0200

    add unclutter and remove warnings
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -3,4 +3,11 @@ docker compose up -d
 
 sleep 10
 
+export DISPLAY=:0
+
+unclutter &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
+
 chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/"
