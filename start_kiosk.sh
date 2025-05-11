1faa245f6b061516ea514fe3298a8b83c48a04ac
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 14:12:06 +0200

    add unclutter
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -5,4 +5,6 @@ sleep 10
 
 export DISPLAY=:0
 
+unclutter &
+
 chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI "http://localhost:9000/"
