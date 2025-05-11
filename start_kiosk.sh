c1eca9574d4f1718fa1527b0ff297f4debbbd789
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-11 13:01:17 +0200

    change start script
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -2,7 +2,4 @@
 docker compose up -d
 sleep 10
 
-startx /usr/bin/openbox-session -- -nocursor &
-sleep 5
-
-DISPLAY=:0 chromium-browser --kiosk --start-fullscreen --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic "http://localhost:9000/"
+ chromium-browser --kiosk --start-maximized --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic "http://localhost:9000/"
