34bc035224877a4277bdc5806e43455697621a98
Steffen Jendrny <steffen.jendrny@iteratec.com>
2025-05-25 15:35:50 +0200

    improve rebuild script
    Signed-off-by: Steffen Jendrny <steffen.jendrny@iteratec.com>

--- scripts/rebuild_images.sh
+++ scripts/rebuild_images.sh
@@ -1,1 +1,8 @@
-docker compose down -v && docker compose up --build -d
+#!/bin/bash
+
+docker compose down
+
+docker volume ls -q | xargs -r docker volume rm
+
+docker compose build --no-cache
+docker compose up -d
